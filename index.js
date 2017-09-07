0d307355c037d4e5155c1d9bbd087bd11335eb01
Ian Mundy <mundycore@gmail.com>
2017-09-06 22:31:02 -0700

    Add arrayOf validator

--- index.js
+++ index.js
@@ -12,19 +12,96 @@ const DtoValidator = function(dto) {
 const DtoTypes = {
   string: {
     name: 'string',
-    check: (value) => stringChecker(value, false),
-    isRequired: { 
+    check: (key, value) => stringChecker('string', key, value, false),
+    isRequired: {
       name: 'string',
-      check: (value) => stringChecker(value, true)
+      check: (key, value) => stringChecker('string', key, value, true)
     },
   },
-  number: {},
-  array: {},
+  number: {
+    name: 'number',
+    check: (key, value) => numberChecker('number', key, value, false),
+    isRequired: {
+      name: 'number',
+      check: (key, value) => numberChecker('number', key, value, true)
+    },
+  },
+  arrayOf: (type) => ({
+    name: 'array',
+    check: (key, value) => arrayOfChecker(type)(`array of ${type}`, key, value, false),
+    isRequired: {
+      name: 'array',
+      check: (key, value) => arrayOfChecker(type)(`array of ${type}`, key, value, true),
+    },
+  }),
   shape: {}
 }
 
-const stringChecker = function(value, isRequired) {
-  return { valid: value && (typeof value === 'string' || value instanceof String), missing: validateIsRequired(value, isRequired) };
+const stringChecker = function(name, key, value, isRequired) {
+  const valid = !value || (typeof value === 'string' || value instanceof String);
+  const missing = validateIsRequired(value, isRequired);
+  const errors = valid ? [] : [ `Expected type ${name} from property ${key}` ];
+  if (missing)
+    errors.push(`Property ${key} is marked as required`);
+
+  return {
+    valid,
+    missing,
+    errors,
+  };
+}
+
+const numberChecker = function(name, key, value, isRequired) {
+  const valid = !value || !stringChecker(name, key, value, isRequired).valid && !isNaN(value);
+  const missing = validateIsRequired(value, isRequired);
+  const errors = valid ? [] : [ `Expected type ${name} from property ${key}` ];
+  if (missing)
+    errors.push(`Property ${key} is marked as required`);
+
+  return {
+    valid,
+    missing,
+    errors,
+  };
+}
+
+const arrayOfChecker = function(typeValidator) {
+  return function(name, key, value, isRequired) {
+    const validArray = !value || Array.isArray(value);
+    const errors = validArray ? [] : [`Expected array for ${key}`];
+    let valid = validArray;
+    let missingValues = false;
+
+    if (validArray && value) {
+      for (let i = 0; i < value.length; i++) {
+        const result = typeValidator.check(key, value[i]);
+        if (result.missing || result.missingValues) {
+          missingValues = true;
+        }
+        if (!result.valid) {
+          valid = false;
+        }
+      }
+      if (!valid) {
+        errors.push(`Expected array of ${typeValidator.name}s for property ${key}`);
+      }
+      if (missingValues) {
+        errors.push(`Property ${key} cannot have undefined values`);
+      }
+    }
+
+    const missing = validateIsRequired(value, isRequired);
+    if (missing) {
+      errors.push(`Property ${key} is marked as required`);
+    }
+
+    return {
+      valid,
+      missing,
+      missingValues,
+      errors,
+    };
+  };
 }
 
 const validateIsRequired = function(value, isRequired) {
@@ -33,18 +110,13 @@ const validateIsRequired = function(value, isRequired) {
 
 const validateDto = function(dtoTypes, dto) {
   const errors = [];
-  for (const key in dto) {
+  for (const key in dtoTypes) {
     validator = dtoTypes[key];
     if (!validator) {
       errors.push(`Sent unexpected property ${key}`);
     } else {
-      const result = validator.check(dto[key]);
-      if (!result.valid) {
-        errors.push(`Expected type ${validator.name} from property ${key}`);
-      }
-      if (result.missing) {
-        errors.push(`Property ${key} is marked as required`);
-      }
+      const result = validator.check(key, dto[key]);
+      errors.push(...result.errors);
     }
   }
   console.log(errors);
@@ -53,12 +125,16 @@ const validateDto = function(dtoTypes, dto) {
 
 const dtoTypeDef = {
   email: DtoTypes.string,
-  password: DtoTypes.string.isRequired
+  password: DtoTypes.string.isRequired,
+  id: DtoTypes.number.isRequired,
+  names: DtoTypes.arrayOf(DtoTypes.string).isRequired
 }
 
 const dto = {
-  email: 'test',
-  password: '',
+  email: null,
+  password: 'test',
+  id: 1.4,
+  names: ['1'],
 }
 
 validateDto (dtoTypeDef, dto);
@@ -66,4 +142,4 @@ validateDto (dtoTypeDef, dto);
 module.exports = {
   DtoTypes,
   DtoValidator
-}
+};
